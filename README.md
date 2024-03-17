903e0f28716648b110a3b518566b58244f28bdb3
Jenny Lind <jeli2308@student.miun.se>
2024-03-17 17:06:01 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Moment 6, _Projekt_
+Den här README-filen har skapats för att beskriva projektets syfte och lista processens olika delar i den ordning som de lösts.
+
+## Projektets syfte
+
+Syftet med uppgiften är omsätta kunskaper från tidigare kursmoment och skapa ett större projekt vilket inkluderar en arbetsprocess i en automatiserad utvecklingsmiljö och versionshantering med Git. En webb-applikation med innehåll baserat på minst två olika JSON-baserade webbtjänster ska skapas med riktning mot en vald målgrupp och ha en tydlig grafisk profil stylad med SASS och CSS-animeringar. 
+
+### Processens olika delar
+
+1. Förbereda utvecklingsmiljön för aktuellt projekt utifrån kunskaper från tidigare kursmoment.
+2. Skapa samtliga filer och mappar som behövs för projektet, inklusive moduler för SASS.
+3. Skapa en enkel index-sida med header, en sök-container och en resultat-container.
+4. Skapa funktionalitet för en filmsök och en resultatlista.
+5. Publicera sidan på Github för versionshantering och på Netlify för att kunna följa autmatisk publicering av nya ändringar. 
+
+#### _Skapad av Jenny Lind, jeli2308_.
---
+++ src/JS/main.js
@@ -0,0 +1,68 @@
+"use strict";
+
+// Deklarerar variabler.
+let resultListEl = document.getElementById("result-list");
+let appInfoEl = document.getElementById("app-info");
+
+// Importerar API-nyckel.
+import { omdbApiKey } from "./apikey.js";
+
+// Händelselyssnare.
+const searchButtonEl = document.getElementById("search-button");
+searchButtonEl.addEventListener("click", getInput);
+
+// Hämtar input från sökfältet och anropar fetch-funktionen.
+function getInput() {
+    const inputValue = document.getElementById("search-input").value;
+    // Anropar funktionen och skickar med inputen. 
+    getData(inputValue);
+};
+
+// Hämtar data från OMDb med "värdet" från sökfältet (10 rader/filmer är default).
+async function getData(inputValue) {
+    try {
+        // Hämtar in film-resultat från sökning.
+        const response = await fetch("http://www.omdbapi.com/?s=" + inputValue + `&type=movie&apikey=${omdbApiKey}`);
+        const data = await response.json();
+
+        // Anropar funktion som visar en lista med filmer. Skickar med datan.
+        showMovieList(data);
+
+    // Felmeddelande som visas i app och i konsollen.
+    } catch (error) {
+        resultListEl.innerHTML = "Something went wrong. Try again!";
+        console.error("Fetch failed. This message was created:", error);
+}
+
+}
+
+// Hämtar in efterfrågad data, sorterar och skriver ut i DOM.
+function showMovieList(data) {
+
+    // Rensar tidigare information.
+    resultListEl.innerHTML = "";
+    appInfoEl.innerHTML = "";
+
+    // Skriver ny info till användaren.
+    appInfoEl.innerHTML = "Click on a movietitle or make a new search!";
+
+    // Kollar om filmtiteln förekommer i API:et och skriver ut en lista.
+    if (data && data.Search) {
+
+        // Loopar genom objekt-array och skriver ut en lista med filmer.
+        data.Search.forEach((movie) => {
+            // Skapar ett listelement för varje film.
+            const movieItem = document.createElement("li");
+            movieItem.innerHTML = `
+                <h3>${movie.Title}, ${movie.Year}</h3>
+            `;
+    
+            // Lägger till listelementen i listan.
+            resultListEl.appendChild(movieItem);
+        });
+   
+    // Meddelande om filmtiteln saknas i API:et.
+    } else {
+        resultListEl.innerHTML = "The movietitle you searched for doesn't exist. Try again with a different title!";
+    }
+};
