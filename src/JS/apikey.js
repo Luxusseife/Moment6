5fb988bd77e2052fe9d291feabbbe3fb46b84766
Jenny Lind <jeli2308@student.miun.se>
2024-03-17 17:40:40 +0100

    Lagt till en ny div i index.html, lagt till en händelselyssnare på listelementen som kör funktionen showMovieInfo. Lagt lite enkel styling i layout.scss.

--- src/JS/apikey.js
+++ src/JS/apikey.js
@@ -1,3 +1,6 @@
 const omdbApiKey = "31553ef8";
 
+const imdbApiKey = "d8919112efmshe3fae74620504eap1a8e47jsn49da76dae0fd";
+
 export {omdbApiKey};
+export {imdbApiKey};
--- src/JS/main.js
+++ src/JS/main.js
@@ -1,11 +1,14 @@
 "use strict";
 
-// Deklarerar variabler.
+// Deklarerar variabler som används i multipla funktioner.
+let searchContainerEl = document.getElementById("search-container");
 let resultListEl = document.getElementById("result-list");
+let resultMovieEl = document.getElementById("result-movie");
 let appInfoEl = document.getElementById("app-info");
 
-// Importerar API-nyckel.
+// Importerar API-nycklar.
 import { omdbApiKey } from "./apikey.js";
+import { imdbApiKey } from "./apikey.js";
 
 // Händelselyssnare.
 const searchButtonEl = document.getElementById("search-button");
@@ -33,13 +36,14 @@ async function getData(inputValue) {
         resultListEl.innerHTML = "Something went wrong. Try again!";
         console.error("Fetch failed. This message was created:", error);
     }
-};
+}
 
 // Hämtar in efterfrågad data, sorterar och skriver ut i DOM.
 function showMovieList(data) {
 
     // Rensar tidigare information.
     resultListEl.innerHTML = "";
+    resultMovieEl.innerHTML = "";
     appInfoEl.innerHTML = "";
 
     // Skriver ny info till användaren.
@@ -61,10 +65,64 @@ function showMovieList(data) {
     
             // Lägger till listelementen i listan.
             resultListEl.appendChild(movieItem);
+
+            // Lägger till en händelselyssnare på varje listelement och skickar med film-ID till ny funktion.
+            movieItem.addEventListener("click", () => showMovieInfo(movie.imdbID));
         });
    
     // Meddelande om filmtiteln saknas i API:et.
     } else {
         resultListEl.innerHTML = "The movietitle you searched for doesn't exist. Try again with a different title!";
+    }
+};
+
+// Hämtar information om en specifik film från IMDb och skriver ut till DOM.
+async function showMovieInfo(movieId) {
+    const url = `https://imdb8.p.rapidapi.com/title/get-overview-details?tconst=${movieId}`;
+    // Alternativ som innefattar API-nyckel.
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': `${imdbApiKey}`,
+            'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+        }
+    };
+
+    try {
+        // Hämtar in film-resultat från sökning.
+        const response = await fetch(url, options);
+        const result = await response.json();
+
+        // Rensar tidigare information.
+        searchContainerEl.innerHTML = "";
+        resultListEl.innerHTML = "";
+        resultMovieEl.innerHTML = "";
+        appInfoEl.innerHTML = "";
+
+        // Skriver ny info till användaren.
+        appInfoEl.innerHTML = `
+            <h1>${result.title.title}</h1>
+        `;
+
+        // Lagrar handling i en variabel då objektets egenskaper varierar.
+        const plotText = result.plotSummary?.text || result.plotOutline?.text;
+
+        // Skapar en artikel med resultat för vald film.
+        const movieDescriptionItem = document.createElement("article");
+        movieDescriptionItem.innerHTML = `
+            <h2>${result.title.year}</h2>
+            <img src="${result.title.image.url}">
+            <h3>Length: ${result.title.runningTimeInMinutes} minutes</h3>
+            <p>${plotText}</p>
+            <h4>Rating on IMDb: ${result.ratings.rating}</h4>
+        `;
+
+        // Lägger till listelementen i listan.
+        resultMovieEl.appendChild(movieDescriptionItem);
+
+    // Felmeddelande som visas i app och i konsollen.
+    } catch (error) {
+        resultMovieEl.innerHTML = "Something went wrong. Try again!";
+        console.error("Fetch failed. This message was created:", error);
     }
 };
